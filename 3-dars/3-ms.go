cd43f25e29c811f450e5df990410ffdd91df5588
SunnatilloTilavov <sunnatillo0987@gmail.com>
2024-01-19 12:16:37 +0500

    yangi

--- 3-dars/3-ms.go
+++ 3-dars/3-ms.go
@@ -3,12 +3,22 @@ package main
 import "fmt"
 
 func main() {
-
- var a,b,d float64
+ var (a,b,d float64 
+ e,r,z float64=18.5,24.9,40
+ g,h,q,w float64=25,29.9,30,39.9)
  fmt.Print("bo'y(sm)= ")
  fmt.Scan(&a)
  fmt.Print("vazn= ")
  fmt.Scan(&b)
  d=(b*100*100)/(a*a)
- fmt.Println("BMI= ",d)
+ fmt.Println("BMI= %f",d)
+ j :=d<e&&e<r
+ k := e<d&&d<r
+ p := g<d&&d<h
+ c := q<d&&d<w
+ x := d>z&&e<r
+ fmt.Println(" vazn yerishmaydi: %v",k)
+ fmt.Println(" normal vazn: %v",p)
+ fmt.Println(" biroz vazn ortiqcha: %v",c)
+ fmt.Println(" xavfli: %v",x,j)
 }
--- 3-dars/4-ms.go
+++ 3-dars/4-ms.go
@@ -3,13 +3,10 @@ package main
 import "fmt"
 
 func main() {
-
- var harf string
-
+ var d string
  fmt.Print("Harf = ")
- fmt.Scan(&harf)
-
- k := (("a" == harf)||("b" == harf)||("c" == harf)||("d" == harf)||("e" == harf)||("f" == harf)||("g" == harf)||("h" == harf)||("i" == harf)||("j" == harf)||("k" == harf)||("l" == harf)||("m" == harf)||("n" == harf)||("o" == harf)||("p" == harf)||("q" == harf)||("r" == harf)||("s" == harf)||("t" == harf)||("u" == harf)||("v" == harf)||("w" == harf)||("x" == harf)||("y" == harf)||("z" == harf))
+ fmt.Scan(&d)
+ k := "a"<=d&&d<="z"
 
  fmt.Printf("kichih harf: %v\n",k)
-}
+} 
