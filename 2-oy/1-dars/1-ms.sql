95892bff09c219a55e4c6fe78e9d82ece5a9ed04
SunnatilloTilavov <sunnatillo0987@gmail.com>
2024-02-16 11:58:30 +0500

    test

--- 2-oy/1-dars/1-ms.sql
+++ 2-oy/1-dars/1-ms.sql
@@ -2,7 +2,7 @@ sudo -u postgres psql
 \l
 \c sqldatabase;
 drop table students ; 
-Create table Students(id Serial PRIMARY KEY,First_name varchar not null,Last_name varchar,Age int,Gender varchar,Score int,Addresss varchar,Email varchar,Phone_number varchar,Date_of_birth date);
+Create table Students(id Serial ,First_name varchar ,Last_name varchar,Age int,Gender varchar,Score int,Addresss varchar,Email varchar,Phone_number varchar,Date_of_birth date);
 \d students;
 Insert into Students(First_name,Last_name,Age,Gender,Score,Addresss,Email,Phone_number,Date_of_birth ) VAlues('A1','b1',11,'male',60,'Toshkent','agr@gmail.com','189382936123','2022-12-12'),
 ('A2','b2',12,'female',75,'Toshkent12','agr31@gmail.com','9189382936123','2023-12-12'),
@@ -16,4 +16,14 @@ update students SET  First_name='A8' where id=2;
 update students SET  Gender='female' where Gender='male';
 update students SET  Score=100 where Score>=100;
 update students SET  Last_name='Rename',First_name='Rename2',Addresss='asdf',Phone_number='9237123d',Date_of_birth='1242-4-25' where id<=4;
-delete from Students where Score<=60
+delete from Students where Score<=60;
+
+
+
+/*Auto use date*/
+CREATE TABLE YTable (
+    id Serial,
+    other_column VARCHAR(255),
+    timestamp_column TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+Insert into YTable(other_column) VAlues ('absd')
---
+++ 2-oy/2-dars/1ms.sql
@@ -0,0 +1,18 @@
+sudo -u postgres psql
+\l
+\c sqldatabase 
+Create table Student(id Serial,name varchar(34),age int,country varchar(34),salary int)
+\d student;
+Insert into Student(name,age,country,salary) Values('abs',12,'tashkent',1234),
+('qwe',15,'asdefr',321),
+('pqw',22,'ihmhmy',168),
+('qad ',32,'oioqwe',574),
+('gabs',12,'wqtqashkent',345),
+('aaa',31,'asdtashkent',4321),
+('adawj',42,'tashkent',423);
+select *from Student;
+select MIN(age) from Student;
+select MAX(salary) from Student;
+select COUNT(id) from Student;
+select AVG(age) from Student;
+select SUM(salary) from Student;
